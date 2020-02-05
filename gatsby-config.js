dd4919c5013bc05ec1656deac6543cba002bec13
Milan Darjanin <milan.darjanin@gmail.com>
2020-02-05 13:47:45 +0100

    Update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,17 +7,17 @@ module.exports = {
   ],
   // Customize your site metadata:
   siteMetadata: {
-    title: `My Blog Title`,
-    author: `My Name`,
-    description: `My site description...`,
+    title: `blog[]`,
+    author: `Milan Darjanin`,
+    description: `Figuring out how to write down my ideas`,
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/gatsbyjs`,
+        url: `https://twitter.com/milandarjanin`,
       },
       {
         name: `github`,
-        url: `https://github.com/gatsbyjs`,
+        url: `https://github.com/darjanin`,
       },
     ],
   },
